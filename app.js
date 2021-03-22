8de7caf39a64d70f2c78ede8cc0f3840d0a7f765
Andre Ferreira <andre.ferreira>
2021-03-22 14:18:30 +0000

    new features

--- app.js
+++ app.js
@@ -92,26 +92,3 @@ document.addEventListener("DOMContentLoaded", () => {
     slider.addEventListener("click", expand);
   });
 });
-
-//? MAIL CHIMP - SUBSCRIPTION PAGE
-
-type = "text/javascript";
-src = "//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"(function (
-  $
-) {
-  window.fnames = new Array();
-  window.ftypes = new Array();
-  fnames[0] = "EMAIL";
-  ftypes[0] = "email";
-  fnames[1] = "FNAME";
-  ftypes[1] = "text";
-  fnames[2] = "LNAME";
-  ftypes[2] = "text";
-  // fnames[3] = "ADDRESS";
-  // ftypes[3] = "address";
-  // fnames[4] = "PHONE";
-  // ftypes[4] = "phone";
-  // fnames[5] = "BIRTHDAY";
-  // ftypes[5] = "birthday";
-})(jQuery);
-var $mcj = jQuery.noConflict(true);
