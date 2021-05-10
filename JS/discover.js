f19d93cdc8826655efed80ff7d82e6ed29a3c386
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2021-05-10 23:28:45 +0100

    tracklists added

--- JS/discover.js
+++ JS/discover.js
@@ -1,4 +1,4 @@
-//? INTERSECTION OBSERVERS
+//? GLOBAL
 const honorableMentions = document.querySelector("aside");
 const artistNum = honorableMentions.querySelectorAll(".artist-num");
 const main = document.querySelector("main");
@@ -7,11 +7,27 @@ const innerWrap = document.querySelector(".inner-wrap");
 const artistSlot = document.querySelectorAll(".artist-slot");
 const artistInfoWrap = honorableMentions.querySelectorAll(".artist-info-wrap");
 const recordTracklist = honorableMentions.querySelector(".record-tracklist");
+const recordMenu = document.querySelector(".record-menu-open");
+const recordAsideChoices = document.querySelector(".side-view");
 
+//? ARTIST / RECORD INFO
 const artistInfo = {
   artist: ["jinjer", "mushroomhead", "love and death"],
   record: ["macro", "a wonderful life", "perfectly preserved"],
   recordTracks: {
+    black_gives_way_to_blue: [
+      "All Secrets Known",
+      "Check My Brain",
+      "Last of My Kind",
+      "Your Decision",
+      "A Looking in View",
+      "When the Sun Rose Again",
+      "Acid Bubble",
+      "Lesson Learned",
+      "Take Her Out",
+      "Private Hell",
+      "Black Gives Way to Blue",
+    ],
     macro: [
       "On The Top",
       "Pit Of Consciousness",
@@ -23,25 +39,55 @@ const artistInfo = {
       "The Prophecy",
       "lainnereP",
     ],
+    a_wonderful_life: [
+      "A Requiem for Tomorrow",
+      "Madness Within",
+      "Seen It All",
+      "The Heresy",
+      "What a Shame",
+      "Pulse",
+      "Carry On",
+      "The Time Has Come",
+      "11th Hour",
+      "I Am the One",
+      "The Flood",
+      "Where the End Begins",
+      "Confutatis",
+    ],
+    perfectly_preserved: [
+      "Infamy",
+      "Tragedy",
+      "Down",
+      "Let Me Love You",
+      "Death of Us",
+      "Slow Fire",
+      "The Hunter",
+      "Lo Lamento",
+      "Affliction",
+      "White Flag",
+    ],
   },
-  releaseDate: ["25 October 2019", "19 June 2020", "12 February 2021"],
-  recordLabel: ["Napalm Records", "Earache Records Ltd"],
+  releaseDate: [
+    "29 September 2009",
+    "25 October 2019",
+    "19 June 2020",
+    "12 February 2021",
+  ],
+  recordLabel: ["Virgin/EMI", "Napalm Records", "Earache Records Ltd"],
 };
 
-// const recordTracks = {
-
-// };
-
+//? OBJECT DESTRUCT
 const { artist, record, recordTracks, releaseDate, recordLabel } = artistInfo;
 
+//? ARTIST PAGE DISPLAY
 artistInfoWrap.forEach((info) => {
   if (info.classList.contains("one")) {
     info.innerHTML = `
     <div class="artist-info-inner-wrap">
       
       <div class="extra-info">
-        <p class="release-info"><i class="fas fa-hand-holding-usd"></i><span class="release-span">${releaseDate[0]}</span></p>
-        <p class="record-info"><i class="fas fa-record-vinyl"></i><span class="record-span">${recordLabel[0]}</span></p>
+        <p class="release-info"><i class="fas fa-hand-holding-usd"></i><span class="release-span">${releaseDate[1]}</span></p>
+        <p class="record-info"><i class="fas fa-record-vinyl"></i><span class="record-span">${recordLabel[1]}</span></p>
       </div>
     </div>
     `;
@@ -73,6 +119,7 @@ artistInfoWrap.forEach((info) => {
   }
 });
 
+//? TRACKLIST INJECTED IN PAGE
 for (i = 1; i < recordTracks.macro.length; i++) {
   const recordTracklistInfo = document.querySelector(
     ".record-tracklist-styling"
@@ -80,6 +127,7 @@ for (i = 1; i < recordTracks.macro.length; i++) {
   recordTracklistInfo.innerHTML += `<div><span class="tracklist-num">${i}</span> - ${recordTracks.macro[i]}</div>`;
 }
 
+//? INTERSECTION OBSERVERS
 let options = {
   threshold: 1,
   rootMargin: "0px",
@@ -116,9 +164,6 @@ function entries(artistNum) {
 artistNum.forEach((entry) => {
   observer2.observe(entry);
 });
-
-const recordMenu = document.querySelector(".record-menu-open");
-const recordAsideChoices = document.querySelector(".side-view");
 
 recordMenu.addEventListener("click", () => {
   if (recordAsideChoices.classList.contains("show")) {
