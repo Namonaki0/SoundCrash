8406fcb853fbb6681a9a6cc283f62c5c34a598cc
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2021-05-15 23:30:29 +0100

    all basic functionality

--- JS/music-player.js
+++ JS/music-player.js
@@ -1,16 +1,63 @@
+//? GLOBAL VARIABLES
 const playBtn = document.querySelector(".play-btn");
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
+let artistPic = document.querySelector(".artist-img-wrapper img");
+let songTitle = document.querySelector(".song-title");
 const audio = document.querySelector("audio");
+const progressBarWrapper = document.querySelector(".progress-bar-wrapper");
+const songMenu = document.querySelector(".song-menu");
 
+//? DATA
 let songs = ["alice_in_chains", "jinjer", "mushroomhead", "love_and_death"];
+let artistPics = ["mp_aic", "mp_jinjer", "mp_mh", "mp_ld"];
+let artists = ["alice in chains", "jinjer", "mushroomhead", "love and death"];
+let songNames = [
+  "private hell",
+  "pit of consciousness",
+  "seen it all",
+  "lo lamento",
+];
 let songIndex = 0;
 
-function loadSong(song) {
-  audio.src = `/Music/${song}.mp3`;
+//? EVENT LISTENERS
+progressBarWrapper.addEventListener("click", songSkip);
+audio.addEventListener("timeupdate", songProgress);
+nextBtn.addEventListener("click", nextSong);
+prevBtn.addEventListener("click", prevSong);
+//? WHEN TRACK FINISHES - SKIP TO NEXT SONG
+audio.addEventListener("ended", nextSong);
+
+//? PROGRESS BAR - SKIP PROGRESS ON CLICK
+function songSkip(e) {
+  const width = e.target.clientWidth;
+  const clickTarget = e.offsetX;
+  const songDuration = audio.duration;
+  audio.currentTime = (clickTarget / width) * songDuration;
 }
 
-loadSong(songs[songIndex]);
+//? PROGRESS BAR - SONG DURATION SYNC
+function songProgress(e) {
+  let progressBar = document.querySelector(".progress-bar");
+  const { currentTime, duration } = e.srcElement;
+  const progressPerc = (currentTime / duration) * 100;
+  progressBar.style.width = `${progressPerc}%`;
+}
+
+//? MUSIC LOADING
+function loadSong(song, pics, artistName, songName) {
+  audio.src = `/Music/${song}.mp3`;
+  artistPic.src = `/Pics/${pics}.jpg`;
+  songTitle.innerHTML = `${songName} - <span class="artist-name">${artistName}</span>`;
+  console.log(songName.innerHTML);
+}
+
+loadSong(
+  songs[songIndex],
+  artistPics[songIndex],
+  artists[songIndex],
+  songNames[songIndex]
+);
 
 //? PLAY CURRENT SONG
 function playSong() {
@@ -32,7 +79,7 @@ function pauseSong() {
   songTitle.classList.remove("show");
 }
 
-//?
+//? PLAY BUTTON TOGGLE - PLAY & PAUSE ICONS
 playBtn.addEventListener("click", () => {
   let playingState = document.querySelector(".play-btn .fas");
   playingState.classList.toggle("fa-pause");
@@ -44,21 +91,31 @@ playBtn.addEventListener("click", () => {
 });
 
 //? SKIP TO FOLLOWING TRACK
-nextBtn.addEventListener("click", () => {
+function nextSong() {
   songIndex++;
   if (songIndex >= songs.length) {
     songIndex = 0;
   }
-  loadSong(songs[songIndex]);
+  loadSong(
+    songs[songIndex],
+    artistPics[songIndex],
+    artists[songIndex],
+    songNames[songIndex]
+  );
   playSong();
-});
+}
 
 //? SKIP TO PREVIOUS TRACK
-prevBtn.addEventListener("click", () => {
+function prevSong() {
   songIndex--;
   if (songIndex < 0) {
     songIndex = songs.length - 1;
   }
-  loadSong(songs[songIndex]);
+  loadSong(
+    songs[songIndex],
+    artistPics[songIndex],
+    artists[songIndex],
+    songNames[songIndex]
+  );
   playSong();
-});
+}
