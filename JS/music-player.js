a86e1f74a4d703754959e81dd08bfc8f6e90bb83
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2021-05-15 10:04:35 +0100

    next button

--- JS/music-player.js
+++ JS/music-player.js
@@ -1,15 +1,16 @@
 const playBtn = document.querySelector(".play-btn");
 const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
 const audio = document.querySelector("audio");
 
 let songs = ["alice_in_chains", "jinjer", "mushroomhead", "love_and_death"];
 let songIndex = 0;
 
-loadSong(songs[songIndex]);
-
 function loadSong(song) {
   audio.src = `/Music/${song}.mp3`;
 }
+
+loadSong(songs[songIndex]);
 
 function playSong() {
   audio.play();
@@ -27,15 +28,29 @@ function pauseSong() {
 
 playBtn.addEventListener("click", (e) => {
   console.log(e.target);
-  let playingState = document.querySelector(".fas");
-  if (playingState.classList.contains("fa-play")) {
-    pauseSong();
-  } else {
+  let playingState = document.querySelector(".play-btn .fas");
+  playingState.classList.toggle("fa-pause");
+  if (playingState.classList.contains("fa-pause")) {
     playSong();
+  } else {
+    pauseSong();
   }
 });
 
 nextBtn.addEventListener("click", () => {
   songIndex++;
+  if (songIndex >= songs.length) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  audio.play();
+});
+
+prevBtn.addEventListener("click", () => {
+  songIndex--;
+  if (songIndex < 0) {
+    songs[songIndex] = -1;
+  }
+  loadSong(songs[songIndex]);
   audio.play();
 });
