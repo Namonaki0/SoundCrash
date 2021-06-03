16f62ccb1345618a9bce284651a36504ee8b29b0
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2021-06-03 23:01:35 +0100

    changes to touring layout

--- JS/upcoming.js
+++ JS/upcoming.js
@@ -4,24 +4,40 @@ const inputValue = document.querySelector("input");
 const searchBtn = searchBar.querySelector("button");
 const key = `Noc1VPzpTVuF1lxH`;
 
-let artist = "";
+let artistSearch = "";
 
 searchBtn.addEventListener("click", () => {
-  artist = inputValue.value;
+  artistInput = inputValue.value.toLocaleLowerCase();
+  eventsOutput.innerHTML = "";
   //   eventsOutput.innerHTML = `<div>hello</div>`;
-  searchArtist(artist);
+  searchArtist(artistInput);
 });
 
-function searchArtist(artist) {
+function searchArtist(artistInput) {
   //   const rob_zombie = `c995a379-60b9-404b-bd97-a7e2de0751d3`;
-  console.log(artist);
+  console.log(artistInput);
   fetch(
-    `https://api.songkick.com/api/3.0/search/artists.json?apikey=${key}&query=${artist}`
+    `https://api.songkick.com/api/3.0/search/artists.json?apikey=${key}&query=${artistInput}`
   )
     .then((data) => data.json())
     .then((data) => {
-      console.log(data.resultsPage.results.artist[0]);
+      console.log(
+        data.resultsPage.results.artist[0],
+        data.resultsPage.results.artist[0].onTourUntil
+      );
       let artist_id = data.resultsPage.results.artist[0].id;
+      let isTouring = data.resultsPage.results.artist[0].onTourUntil;
+
+      //? ARTIST NOT TOURING
+      if (isTouring === null) {
+        console.log("artist not touring at present");
+        eventsOutput.innerHTML += `
+         <div class="event-info">
+            <div class="no-tour">artist not touring at present</div>
+        </div>
+        
+        `;
+      }
 
       tourDates(artist_id);
     });
