d8732ad70b76c634610d13b421691ea66934c704
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2021-06-08 00:25:06 +0100

    async await refactoring in discover page

--- JS/discover.js
+++ JS/discover.js
@@ -1,6 +1,5 @@
 //? GLOBAL
 const honorableMentions = document.querySelector("aside");
-// const artistNum = honorableMentions.querySelectorAll(".artist-num");
 const main = document.querySelector("main");
 const numSystem = main.querySelectorAll(".num-system");
 const innerWrap = document.querySelector(".inner-wrap");
@@ -9,7 +8,7 @@ const artistInfoWrap = honorableMentions.querySelector(".artist-info-wrap");
 const recordTracklist = honorableMentions.querySelector(".record-tracklist");
 const recordMenu = document.querySelector(".record-menu-open");
 const recordAsideChoices = document.querySelector(".side-view");
-const records = document.querySelectorAll(".side-view-img-wrap");
+const recordWrap = document.querySelectorAll(".side-view-img-wrap");
 const artistCover = document.querySelector(".img-wrap img");
 const recordTitle = document.querySelector(".record-title");
 const artistTitle = document.querySelector(".artist-title");
@@ -50,6 +49,14 @@ document.addEventListener("DOMContentLoaded", async () => {
     );
     recordTracklistInfo.innerHTML += `<div><span class="tracklist-num">${i}</span> - ${recordTracks.black_gives_way_to_blue[i]}</div>`;
   }
+
+  recordMenuChoice({
+    records,
+    recordCovers,
+    recordTracks,
+    releaseDate,
+    recordLabel,
+  });
 });
 
 //? RECORD OPTIONS MENU
@@ -64,28 +71,35 @@ recordMenu.addEventListener("click", (e) => {
   }
 });
 
-//? INDIVIDUAL RECORD INFORMATION
-records.forEach((each_record) => {
-  each_record.addEventListener("click", (e) => {
-    const dataSet = e.target.dataset.value;
-    const title = document.querySelector(".inner-wrap h2");
-    const albumCover = document.querySelector(".img-wrap img");
-    const imgWrap = document.querySelector(".img-wrap");
-    const recordTracklistInfo = document.querySelector(
-      ".record-tracklist-styling"
-    );
+function recordMenuChoice({
+  records,
+  recordCovers,
+  recordTracks,
+  releaseDate,
+  recordLabel,
+}) {
+  //? INDIVIDUAL RECORD INFORMATION
+  recordWrap.forEach((each_record) => {
+    each_record.addEventListener("click", async (e) => {
+      const dataSet = await e.target.dataset.value;
+      const title = document.querySelector(".inner-wrap h2");
+      const recordCover = document.querySelector(".img-wrap img");
+      const imgWrap = document.querySelector(".img-wrap");
+      const recordTracklistInfo = document.querySelector(
+        ".record-tracklist-styling"
+      );
 
-    //? EMPTY TRACKLIST FOR NEXT CHOICE
-    if (e.target != dataSet) {
-      recordTracklistInfo.innerHTML = "";
-    }
-    switch (dataSet) {
-      case "one":
-        e.target.src = recordCovers[0];
-        recordCover.src = e.target.src;
-        recordTitle.innerHTML = `${records[0]}`;
-        artistTitle.innerHTML = `${artists[0]}`;
-        artistInfoWrap.innerHTML = `
+      //? EMPTY TRACKLIST FOR NEXT CHOICE
+      if (e.target != dataSet) {
+        recordTracklistInfo.innerHTML = "";
+      }
+      switch (dataSet) {
+        case "one":
+          e.target.src = recordCovers[0];
+          recordCover.src = e.target.src;
+          recordTitle.innerHTML = `${records[0]}`;
+          artistTitle.innerHTML = `${artists[0]}`;
+          artistInfoWrap.innerHTML = `
           <div class="artist-info-inner-wrap">
             <div class="extra-info">
               <p class="release-info"><i class="fas fa-hand-holding-usd"></i><span class="release-span">${releaseDate[0]}</span></p>
@@ -93,19 +107,19 @@ records.forEach((each_record) => {
             </div>
           </div>
         `;
-        for (i = 1; i < recordTracks.black_gives_way_to_blue.length; i++) {
-          const recordTracklistInfo = document.querySelector(
-            ".record-tracklist-styling"
-          );
-          recordTracklistInfo.innerHTML += `<div><span class="tracklist-num">${i}</span> - ${recordTracks.black_gives_way_to_blue[i]}</div>`;
-        }
-        break;
-      case "two":
-        e.target.src = recordCovers[1];
-        recordCover.src = e.target.src;
-        recordTitle.innerHTML = `${records[1]}`;
-        artistTitle.innerHTML = `${artists[1]}`;
-        artistInfoWrap.innerHTML = `
+          for (i = 1; i < recordTracks.black_gives_way_to_blue.length; i++) {
+            const recordTracklistInfo = document.querySelector(
+              ".record-tracklist-styling"
+            );
+            recordTracklistInfo.innerHTML += `<div><span class="tracklist-num">${i}</span> - ${recordTracks.black_gives_way_to_blue[i]}</div>`;
+          }
+          break;
+        case "two":
+          e.target.src = recordCovers[1];
+          recordCover.src = e.target.src;
+          recordTitle.innerHTML = `${records[1]}`;
+          artistTitle.innerHTML = `${artists[1]}`;
+          artistInfoWrap.innerHTML = `
           <div class="artist-info-inner-wrap">
             <div class="extra-info">
               <p class="release-info"><i class="fas fa-hand-holding-usd"></i><span class="release-span">${releaseDate[1]}</span></p>
@@ -113,19 +127,19 @@ records.forEach((each_record) => {
             </div>
           </div>
         `;
-        for (i = 1; i < recordTracks.macro.length; i++) {
-          const recordTracklistStyling = document.querySelector(
-            ".record-tracklist-styling"
-          );
-          recordTracklistStyling.innerHTML += `<div><span class="tracklist-num">${i}</span> - ${recordTracks.macro[i]}</div>`;
-        }
-        break;
-      case "three":
-        e.target.src = recordCovers[2];
-        recordCover.src = e.target.src;
-        recordTitle.innerHTML = `${records[2]}`;
-        artistTitle.innerHTML = `${artists[2]}`;
-        artistInfoWrap.innerHTML = `
+          for (i = 1; i < recordTracks.macro.length; i++) {
+            const recordTracklistStyling = document.querySelector(
+              ".record-tracklist-styling"
+            );
+            recordTracklistStyling.innerHTML += `<div><span class="tracklist-num">${i}</span> - ${recordTracks.macro[i]}</div>`;
+          }
+          break;
+        case "three":
+          e.target.src = recordCovers[2];
+          recordCover.src = e.target.src;
+          recordTitle.innerHTML = `${records[2]}`;
+          artistTitle.innerHTML = `${artists[2]}`;
+          artistInfoWrap.innerHTML = `
           <div class="artist-info-inner-wrap">
             <div class="extra-info">
               <p class="release-info"><i class="fas fa-hand-holding-usd"></i><span class="release-span">${releaseDate[2]}</span></p>
@@ -133,19 +147,19 @@ records.forEach((each_record) => {
             </div>
           </div>
         `;
-        for (i = 1; i < recordTracks.a_wonderful_life.length; i++) {
-          const recordTracklistInfo = document.querySelector(
-            ".record-tracklist-styling"
-          );
-          recordTracklistInfo.innerHTML += `<div><span class="tracklist-num">${i}</span> - ${recordTracks.a_wonderful_life[i]}</div>`;
-        }
-        break;
-      case "four":
-        e.target.src = recordCovers[3];
-        recordCover.src = e.target.src;
-        recordTitle.innerHTML = `${records[3]}`;
-        artistTitle.innerHTML = `${artists[3]}`;
-        artistInfoWrap.innerHTML = `
+          for (i = 1; i < recordTracks.a_wonderful_life.length; i++) {
+            const recordTracklistInfo = document.querySelector(
+              ".record-tracklist-styling"
+            );
+            recordTracklistInfo.innerHTML += `<div><span class="tracklist-num">${i}</span> - ${recordTracks.a_wonderful_life[i]}</div>`;
+          }
+          break;
+        case "four":
+          e.target.src = recordCovers[3];
+          recordCover.src = e.target.src;
+          recordTitle.innerHTML = `${records[3]}`;
+          artistTitle.innerHTML = `${artists[3]}`;
+          artistInfoWrap.innerHTML = `
           <div class="artist-info-inner-wrap">
             <div class="extra-info">
               <p class="release-info"><i class="fas fa-hand-holding-usd"></i><span class="release-span">${releaseDate[3]}</span></p>
@@ -153,18 +167,19 @@ records.forEach((each_record) => {
             </div>
           </div>
         `;
-        for (i = 1; i < recordTracks.perfectly_preserved.length; i++) {
-          const recordTracklistInfo = document.querySelector(
-            ".record-tracklist-styling"
-          );
-          recordTracklistInfo.innerHTML += `<div><span class="tracklist-num">${i}</span> - ${recordTracks.perfectly_preserved[i]}</div>`;
-        }
-        break;
-      default:
-        return;
-    }
+          for (i = 1; i < recordTracks.perfectly_preserved.length; i++) {
+            const recordTracklistInfo = document.querySelector(
+              ".record-tracklist-styling"
+            );
+            recordTracklistInfo.innerHTML += `<div><span class="tracklist-num">${i}</span> - ${recordTracks.perfectly_preserved[i]}</div>`;
+          }
+          break;
+        default:
+          return;
+      }
+    });
   });
-});
+}
 
 //? INTERSECTION OBSERVERS
 let options = {
@@ -187,19 +202,3 @@ function entries(numSystem) {
 numSystem.forEach((num) => {
   observer1.observe(num);
 });
-
-// observer2 = new IntersectionObserver(entries, options);
-
-// function entries(artistNum) {
-//   artistNum.forEach((entry) => {
-//     if (entry.isIntersecting && entry.intersectionRatio > 0) {
-//       entry.target.classList.add("special");
-//     } else {
-//       entry.target.classList.remove("special");
-//     }
-//   });
-// }
-
-// artistNum.forEach((entry) => {
-//   observer2.observe(entry);
-// });
