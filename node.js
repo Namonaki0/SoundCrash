310a56764940557048ee5045f81eb0a854c032d1
Andre Ferreira <49961945+Namonaki0@users.noreply.github.com>
2021-06-16 23:05:22 +0100

    database

--- node.js
+++ node.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("THIS IS THE PAGE");
@@ -11,3 +12,11 @@ app.get("/", (req, res) => {
 app.listen(3000);
 
 //? ---------------------------------------------------
+
+mongoose.connect(
+  "mongodb+srv://sccluster.l8n0h.mongodb.net/SoundCrashDB",
+  { useNewUrlParser: true },
+  () => {
+    console.log("CONNECTED TO DATABASE");
+  }
+);
